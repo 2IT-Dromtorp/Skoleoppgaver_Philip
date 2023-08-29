2ae3241a9ccce05ea51c405c98a5ca948eecd728
phibey06 <116543645+phibey06@users.noreply.github.com>
2023-08-29 12:47:05 +0200

    test

--- react_intro/react_intro/src/App.js
+++ react_intro/react_intro/src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+/*import Login from './login';*/
+/*import AdminPanel from './admin';*/
+
 
 function App() {
+  let isLoggedIn;
+  let content;
+  isLoggedIn = false;
+
+  if (isLoggedIn) {
+    content = <AdminPanel />;
+  } else {
+    content = <Login />;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
+        <h1>react intro</h1>
+          <input></input>
+          <input></input>
+          <button onClick={Login}>Login</button>
+          {content}
+
       </header>
     </div>
   );
