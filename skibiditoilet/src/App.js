56f80b168b63fe173b4d9134bc18be0ef13f2dd1
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-01-29 10:26:33 +0100

    skibidi my friend

--- skibiditoilet/src/App.js
+++ skibiditoilet/src/App.js
@@ -1,10 +1,9 @@
 import './App.css';
-import skibidiimg from "./images/eb7e2e66b8df91e98dff3fd83dbb156b.900x900x1.jpg";
 
 function App() {
   return (
   <div id='main'>
-    <img src={skibidiimg} width="100%" />
+    <img src="https://i.ibb.co/bJzftqR/eb7e2e66b8df91e98dff3fd83dbb156b-900x900x1.jpg" width="100%" />
   </div>
   )
 }
