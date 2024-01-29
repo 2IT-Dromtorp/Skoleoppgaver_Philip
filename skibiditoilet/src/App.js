a3a58b228c43c41f014005f23df068b6db53299b
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-01-29 10:42:21 +0100

    skibidi brær

--- skibiditoilet/src/App.js
+++ skibiditoilet/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 
 function App() {
+  const skibidi = () => {
+    alert("Skibidi dop dop dop dop yes yes yes skibidi dip skibido dop skibidi yes yes yes.")
+    window.location.href="https://www.youtube.com/playlist?list=PL-ZXraMeHBPJHXBhrNowJaQslyqtUg-tZ"
+  }
   return (
-  <div id='main'>
+  <div id='main' onClick={skibidi}>
     <img src="https://i.ibb.co/bJzftqR/eb7e2e66b8df91e98dff3fd83dbb156b-900x900x1.jpg" width="100%" />
   </div>
   )
