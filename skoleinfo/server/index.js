ce6faec02561979ab255693365af5b44d27a70d8
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-02-07 09:23:43 +0100

    sex sex sex

--- skoleinfo/server/index.js
+++ skoleinfo/server/index.js
@@ -4,6 +4,10 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static("build"));
 
+app.get("/klubber.json", (req, res) => {
+    res.sendFile(__dirname + "/build/klubber.js");
+});
+
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
