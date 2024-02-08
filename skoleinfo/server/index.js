fe855cf70e49b5ccf056120bb94dde627006db82
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-02-08 12:49:34 +0100

    guh

--- skoleinfo/server/index.js
+++ skoleinfo/server/index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
+const path = require("node:path")
 
 app.use(express.static("build"));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
 })
+
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve("./build/index.html"));
+});
