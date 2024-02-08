4bd57c521406ad1c90a78fed002cf1f7902e8c95
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-02-08 13:10:34 +0100

    test 2

--- skoleinfo/server/index.js
+++ skoleinfo/server/index.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-//const path = require("node:path")
+const path = require("node:path")
 
 app.use(express.static("build"));
-
-
 
 app.get("/buss", (req, res) => {
     res.sendFile(path.resolve("./build/index.html"));
