a20070628cf38f5d2f93d90b93f796b7330b69bc
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-02-08 13:06:56 +0100

    testing routing

--- skoleinfo/server/index.js
+++ skoleinfo/server/index.js
@@ -1,14 +1,18 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 8080
-const path = require("node:path")
+//const path = require("node:path")
 
 app.use(express.static("build"));
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
 
-app.get("*", (req, res) => {
+
+app.get("/buss", (req, res) => {
     res.sendFile(path.resolve("./build/index.html"));
 });
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  })
+
+//gub hueuaddas
