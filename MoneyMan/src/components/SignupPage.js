52302066cf9068fd99645cbd394bca8b1ad7eb58
phibey06 <116543645+phibey06@users.noreply.github.com>
2024-03-06 09:20:51 +0100

    mongodb mongodb mongodb mongodb

--- MoneyMan/src/components/SignupPage.js
+++ MoneyMan/src/components/SignupPage.js
@@ -10,6 +10,19 @@ function Signup() {
   const navigate = useNavigate();
 
   const handleSignUp = async () => {
+
+    username = username.toLowerCase();
+
+    if (username === '') {
+      toast.error('Vennligst skriv inn et brukernavn');
+    } else if (username.split('').includes(' ')) {
+      toast.error("DU KAN IKKE HA MELLOMROM I BRUKERNAVN!!!!!!!!!!!!!!!");
+    } else if (email === '') {
+      toast.error('Vennligst skriv inn en email');
+    } else if (password === '') {
+      toast.error('Vennligst skriv inn et passord');
+    }
+
     try {
       const response = await fetch('/signup', {
         method: 'POST',
@@ -37,7 +50,7 @@ function Signup() {
   return (
     <div id="mainWindow">
       <div id="loginSection">
-        <img id="logo" src={Logo} alt='logo'/>
+        <img id="logo" src={Logo} alt='logo' />
         <div id="inputWrapper">
           <input placeholder="Brukernavn" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
