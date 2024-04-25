a91bd4f1ef7368055f7fd60268fc11f4d0054b28
Philip Beyer <philip.beyer06@gmail.com>
2024-04-25 08:21:37 +0200

    Pappa

--- blitztracker/src/App.js
+++ blitztracker/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './components/_global/global.css';
+import Template from './components/template/template';
+import Landing from './components/landing/landing';
+import Player from './components/player/player';
+import Clan from './components/clan/clan';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Template />}>
+          <Route index element={<Landing />} />
+          <Route path="/player/:region/:accountid" element={<Player />} />
+          <Route path="/clan/:region/:clanid" element={<Clan />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ blitztracker/src/components/player/player.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Player() {
+    return (
+        <div id='content'>
+
+        </div>
+    )
+}
+
+export default Player
